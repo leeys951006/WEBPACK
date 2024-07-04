36374bcab32f04276e1fa3d2d7376460d91a2457
이연승 <leeys951006@gmail.com>
2024-07-04 11:06:05 +0900

    menu.ts 내용 추가

---
+++ dist/menu.js
@@ -0,0 +1,4 @@
+"use strict";
+
+var root = document.getElementById('root');
+console.log(root);
---
+++ src/menu.ts
@@ -0,0 +1,17 @@
+const root = document.getElementById('root')!;
+
+const anchorTags = (): string => {
+  let result = '<a href="#">메뉴</a>';
+  return result;
+};
+
+const liTags = (children: string): string => {
+  let result = `<li>${children}</li>`;
+  return result;
+};
+
+root.innerHTML = `
+<ul>
+  ${liTags(anchorTags())}
+</ul>
+`;
